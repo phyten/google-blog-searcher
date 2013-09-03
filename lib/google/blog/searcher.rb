0937600ca64c254ecdcb1b9aab4ad3db128605b7
phyten <phyten.obr@gmail.com>
2013-09-03 21:11:06 +0900

    Google::Blog::Searcher::Coreのfirst commit

--- lib/google/blog/searcher.rb
+++ lib/google/blog/searcher.rb
@@ -1,9 +1,18 @@
 require "google/blog/searcher/version"
 
+require 'rubygems'
+require "rss/maker"
+require 'open-uri'
+require "kconv"
+require 'rss'
+
+
 module Google
   module Blog
     module Searcher
-      # Your code goes here...
+      class Core
+        
+      end
     end
   end
 end
