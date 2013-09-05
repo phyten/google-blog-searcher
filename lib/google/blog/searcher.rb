353d3dae9ff837031b4bc7d6c9a89bacee59e1c2
phyten <phyten.obr@gmail.com>
2013-09-05 18:53:04 +0900

    parse

--- lib/google/blog/searcher.rb
+++ lib/google/blog/searcher.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require "google/blog/searcher/version"
 
 require 'rubygems'
@@ -5,6 +6,7 @@ require "rss/maker"
 require 'open-uri'
 require "kconv"
 require 'rss'
+require 'mechanize'
 
 
 module Google
@@ -12,6 +14,36 @@ module Google
     module Searcher
       class Core
         
+      end
+      class Parser
+        def self.parse(words=[], start=1)
+          _parse("https://www.google.co.jp/search?tbm=blg&hl=ja&q=#{words.join(' ')}&output=rss&start=#{start}")
+        end
+        private
+        def self._parse(url)
+          # URLへアクセスしページを取得
+          begin
+            useragent = 'Mac Safari'
+            mechanize = Mechanize.new
+            mechanize.read_timeout = 20
+            mechanize.max_history = 10
+            mechanize.user_agent_alias = useragent
+            page = mechanize.get(url)
+            content = page.content.to_s.toutf8            
+          rescue Exception
+            return nil
+          end
+          # XMLをパース
+          rss = nil
+          begin
+            rss = RSS::Parser.parse(content)
+          rescue RSS::InvalidRSSError
+            rss = RSS::Parser.parse(content, false)
+          end
+          rss.items.map do |item|
+            item.title.exclude_tag
+          end
+        end
       end
     end
   end
--- spec/core_spec.rb
+++ spec/core_spec.rb
@@ -1,5 +1,14 @@
-
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'bundler/setup'
+$:.unshift(File.dirname(__FILE__) + '/../lib/')
+require 'google/blog/searcher'
 
 describe Google::Blog::Searcher::Parser do
-  
+  describe '#parse' do
+    let(:parser){ Google::Blog::Searcher::Parser }
+    it "can parse with words" do
+      parser.parse(["coffee", "delicious"]) {should be_kind_of Array}
+    end
+  end
 end
